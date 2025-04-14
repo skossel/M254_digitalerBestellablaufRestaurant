40a923ebc77b98cc5c8a089f76b2d27349e72112
Sandro Kossel <sandro.kossel@ergon.ch>
2025-04-14 10:16:18 +0200

    changes

---
+++ Project/hand_over_buzzer.py
@@ -0,0 +1,26 @@
+from camunda.external_task.external_task import ExternalTask, TaskResult
+from camunda.external_task.external_task_worker import ExternalTaskWorker
+
+config = {
+    "maxTasks": 5,
+    "lockDuration": 10000,
+    "asyncResponseTimeout": 5000,
+    "retries": 3,
+    "retryTimeout": 5000,
+    "sleepSeconds": 30
+}
+
+def main():
+    worker = ExternalTaskWorker(worker_id="1", config=config)
+    worker.subscribe("hand_over_buzzer", handle_buzzer)
+
+def handle_buzzer(task: ExternalTask) -> TaskResult:
+    print(f"TaskID ist {task.get_task_id()}")
+
+    orderNumber = task.get_variable("orderNumber")
+    print(f"***** Buzzer '{orderNumber}' wurde vorgegeben")
+
+    return task.complete({"orderNumber": orderNumber})
+
+if __name__ == '__main__':
+    main()
